1bd7a5c95822fab599d52a45d826bb807b193b3f
Charlon Trinidad <charlon@cloud-edge.com.au>
2026-08-20 16:00:20 +1000

    Latest audio changes

--- src/App.tsx
+++ src/App.tsx
@@ -192,7 +192,7 @@ function App() {
   if (mediaItems.length === 0) return;
 
   setPresentationMode(true);
-  setSoundEnabled(true);
+  setSoundEnabled(false);
 
   const video = videoRef.current;
 
