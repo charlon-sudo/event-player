320310c7d998a724fd3ba1606b05b2052f503ff7
Charlon Trinidad <charlon@cloud-edge.com.au>
2026-08-20 11:29:39 +1000

    SharePoint folder listing working

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,81 @@
 import "./App.css";
-import { useState } from "react";
 import { useMsal } from "@azure/msal-react";
 
 function App() {
   const { instance, accounts } = useMsal();
 
-  const [graphResult, setGraphResult] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-
   const signIn = async () => {
     await instance.loginRedirect({
-      scopes: ["User.Read", "Files.Read.All", "Sites.Read.All"],
+      scopes: ["User.Read"],
     });
   };
 
   const signOut = async () => {
     await instance.logoutRedirect();
   };
-
-  const testGraph = async () => {
-    if (accounts.length === 0) {
-      return;
-    }
-
-    setLoading(true);
-    setGraphResult("");
-
-    try {
-      const tokenResponse = await instance.acquireTokenSilent({
-        scopes: ["User.Read", "Files.Read.All", "Sites.Read.All"],
+  const loadMedia = async () => {
+  try {
+    const tokenResponse =
+      await instance.acquireTokenSilent({
+        scopes: [
+          "User.Read",
+          "Files.Read.All",
+          "Sites.Read.All",
+        ],
         account: accounts[0],
       });
 
-      const response = await fetch("https://graph.microsoft.com/v1.0/me", {
+    const response = await fetch(
+      "https://graph.microsoft.com/v1.0/sites/grandhotelwarrandyte.sharepoint.com:/sites/GHW",
+      {
         headers: {
           Authorization: `Bearer ${tokenResponse.accessToken}`,
         },
-      });
-
-      if (!response.ok) {
-        throw new Error(`Graph returned ${response.status}: ${response.statusText}`);
       }
+    );
 
-      const data = await response.json();
+    const data = await response.json();
+console.log(data);
+    console.log("SITE DATA", data);
 
-      setGraphResult(JSON.stringify(data, null, 2));
-    } catch (error) {
-      const message =
-        error instanceof Error ? error.message : "Unable to call Microsoft Graph.";
+const driveResponse = await fetch(
+  `https://graph.microsoft.com/v1.0/sites/${data.id}/drive`,
+  {
+    headers: {
+      Authorization: `Bearer ${tokenResponse.accessToken}`,
+    },
+  }
+);
 
-      setGraphResult(message);
-    } finally {
-      setLoading(false);
-    }
-  };
+const driveData = await driveResponse.json();
+
+console.log("DRIVE DATA", driveData);
+
+console.log("DRIVE DATA", driveData);
+
+const folderResponse = await fetch(
+  `https://graph.microsoft.com/v1.0/drives/${driveData.id}/root:/Functions/Functions%20AV%20Uploads/Caledonia%20Room/Event%201:/children`,
+  {
+    headers: {
+      Authorization: `Bearer ${tokenResponse.accessToken}`,
+    },
+  }
+);
+
+const folderData = await folderResponse.json();
+
+console.log("FOLDER DATA", folderData);
+
+const fileNames = folderData.value
+  .map((file: any) => file.name)
+  .join("\n");
+
+alert(fileNames);
+  } catch (error) {
+    console.error(error);
+    alert("Failed");
+  }
+};
 
   return (
     <div className="app">
@@ -63,28 +85,30 @@ function App() {
         {accounts.length > 0 ? (
           <>
             <h2>Welcome</h2>
+
             <p>{accounts[0].username}</p>
 
-            <div className="buttonRow">
-              <button className="signInButton" onClick={testGraph}>
-                Test Microsoft Graph
-              </button>
+           <div className="buttonRow">
+  <button
+  className="signInButton"
+  onClick={loadMedia}
+>
+  Load SharePoint Media
+</button>
 
-              <button className="secondaryButton" onClick={signOut}>
-                Sign out
-              </button>
-            </div>
-
-            {loading && <p>Loading...</p>}
-
-            {graphResult && (
-              <pre className="resultBox">
-                {graphResult}
-              </pre>
-            )}
+  <button
+    className="secondaryButton"
+    onClick={signOut}
+  >
+    Sign Out
+  </button>
+</div>
           </>
         ) : (
-          <button className="signInButton" onClick={signIn}>
+          <button
+            className="signInButton"
+            onClick={signIn}
+          >
             Sign in with Microsoft
           </button>
         )}
